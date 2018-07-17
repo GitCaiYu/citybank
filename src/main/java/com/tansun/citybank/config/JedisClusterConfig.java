88c0365d2c8eb64a0848598f0605e2bd724eb511
GitCaiYu <41113677+GitCaiYu@users.noreply.github.com>
2018-07-17 12:06:20 +0800

    新增cache缓存与redis整合

--- src/main/java/com/tansun/citybank/config/JedisClusterConfig.java
+++ src/main/java/com/tansun/citybank/config/JedisClusterConfig.java
@@ -1,6 +1,8 @@
 package com.tansun.citybank.config;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
@@ -8,12 +10,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedisPool;
 
 @Configuration
 public class JedisClusterConfig {
 
-	@Value("${spring.redis.clusterNodes}")
+	@Value("${spring.jedis.clusterNodes}")
 	private String clusterNodes;
 
 	@Value("${spring.redis.password}")
@@ -27,6 +33,19 @@ public class JedisClusterConfig {
 			nodes.add(new HostAndPort(ipPortPair[0].trim(), Integer.valueOf(ipPortPair[1].trim())));
 		}
 		return new JedisCluster(nodes, 3000, 3000, 1, password, new GenericObjectPoolConfig());
+	}
+
+	public void poolInit() {
+		Jedis jedis = new Jedis();
+		JedisPool jedisPool = new JedisPool();
+		List<JedisShardInfo> shards = new ArrayList<>();
+		String[] serverArray = clusterNodes.split(",");
+		for (String ipPort : serverArray) {
+			String[] ipPortPair = ipPort.split(":");
+			new JedisShardInfo(ipPortPair[0].trim(), Integer.valueOf(ipPortPair[1].trim())).setPassword(password);;
+		}
+		ShardedJedisPool shardedJedisPool = new ShardedJedisPool(new GenericObjectPoolConfig(), shards);
+//		shardedJedisPool.
 	}
 
 }
