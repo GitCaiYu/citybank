506ec2628503c0640e3e0e9ad7e2f8ac79510e26
Administrator <caiyu@tansun.com.cn>
2018-09-09 14:26:12 +0800

    使用云配置并新增自定义线程池

--- src/main/java/com/tansun/citybank/CitybankApplication.java
+++ src/main/java/com/tansun/citybank/CitybankApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableCaching
 @ComponentScan(basePackages = { "com.tansun" })
 @MapperScan("com.tansun.citybank.dao")
+@RefreshScope
 public class CitybankApplication {
 
 	@LoadBalanced
